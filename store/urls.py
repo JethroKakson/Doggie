4b0bb62274e5b1908f3bb9bb1f060cac63db0d54
JethroKakson <jethrokakson@gmail.com>
2024-03-28 13:22:26 +0300

    login and logout for admin

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'), # we name them login_user because django also has a function called login.
+    path('logout/', views.logout_user, name='logout'),
 ]
