3704caee4c33badc15fe209b6bfd881e951bfe4b
JethroKakson <jethrokakson@gmail.com>
2024-04-11 13:32:48 +0300

    adding quantity to cart

--- cart/cart.py
+++ cart/cart.py
@@ -13,14 +13,17 @@ class Cart():
         # make sure cart is available on all pages
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
     # logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+
         self.session.modified = True
 
 
@@ -34,3 +37,8 @@ class Cart():
         products = Product.objects.filter(id__in=product_ids)
         # return the looked up products
         return products
+
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,8 @@ def cart_summary(request):
     cart = Cart(request)
     # getting the products from the cart
     cart_products = cart.get_products
-    return render(request, 'cart_summary.html', {"cart_products": cart_products})
+    quantities = cart.get_quants
+    return render(request, 'cart_summary.html', {"cart_products": cart_products, 'quantities': quantities})
 
 
 def cart_add(request):
@@ -19,10 +20,11 @@ def cart_add(request):
     if request.POST.get('action') == 'post': # this is because in ajax we used the lowercase post for the action
 #         get stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
 #         lookup product in database
         product = get_object_or_404(Product, id=product_id)
 #     save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
 # Get cart quantity
         cart_quantity = cart.__len__()
